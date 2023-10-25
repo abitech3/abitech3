3960e271361307487950ffce3519952714217963
adepoju hammed <adepojuhameed3@gmail.com>
2023-10-25 10:49:05 +0100

    change in code

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,11 @@
-function calculateDayBetweenDate(){}
+// create function for erro page
+
+function errorPage() {
+    var error = document.getElementById("error");
+    error.innerHTML = "Error: Page not found";
+    error.style.color = "red";
+    error.style.fontSize = "50px";
+    error.style.fontWeight = "bold";
+    error.style.textAlign = "center";
+    error.style.marginTop = "200px";
+}
