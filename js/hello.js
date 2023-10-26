194b9e92443de51c112f578d0bef29683a7e5ec3
adepoju hammed <adepojuhameed3@gmail.com>
2023-10-26 03:32:23 +0100

    add changes to js

--- js/hello.js
+++ js/hello.js
@@ -9,3 +9,26 @@ function errorPage() {
     error.style.textAlign = "center";
     error.style.marginTop = "200px";
 }
+
+// find all images without alternate text
+// and give them a red border
+function process() {
+    var images = document.getElementsByTagName("img");
+    for (var i = 0; i < images.length; i++) {
+        if (images[i].alt == "") {
+            images[i].style.border = "3px solid red";
+        }
+    }
+}
+
+// display time in html
+
+
+
+// Return the current time
+function getTime() {
+    var now = new Date();
+    var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+    return time;
+}
+
